e821e05f68cc00ec90d0aabf9c22c249b153956d
BobTomBob <josephbsilvi@gmail.com>
2017-07-24 11:06:38 -0700

    Person

---
+++ Person/src/person/Person.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("My name is Joseph Silvi");
+       System.out.println("I like cars");
+      System.out.println("I am from Sonoma");
+       System.out.println("I am Filipino");
+        System.out.println("Computers are cool");
+      }
+    
+    
+   
+    
+}
