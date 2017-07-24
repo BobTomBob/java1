aaabf0955dd20000b27361a1e91361f0d270b312
BobTomBob <josephbsilvi@gmail.com>
2017-07-24 11:32:05 -0700

    Person2

---
+++ Person 2/src/person/pkg2/Person2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person.pkg2;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Person2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int age = 14;
+        double height = 5.75;
+        String firstName = "Joseph";
+        String lastName = "Silvi";
+        boolean isAStudent = true;
+        System.out.println("My name is "+firstName+ " " + lastName);
+        Syste.out.println ()
+    }
+    
+}
