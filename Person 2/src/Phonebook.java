90d49a19a8209520e5bcd31e02d3cba03f4f1edd
BobTomBob <josephbsilvi@gmail.com>
2017-07-25 16:25:37 -0700

    Phonebook

---
+++ Person 2/src/Phonebook.java
@@ -0,0 +1,29 @@
+
+import person.pkg2.Person2;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Phonebook {
+    Person2[] PBList;
+    int size;
+    
+    public void Phonebook() {
+        size = 0;
+        PBList = new Person2[100];
+    }
+            public void addPerson(Person2 p){
+                PBList[size]=p;
+                size++;
+            }
+            public void countPeople(){
+                    for(int )
+            
+}
