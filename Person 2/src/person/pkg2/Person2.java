8e693d270d4e3a04918237a051cca2424fc8547d
BobTomBob <josephbsilvi@gmail.com>
2017-07-25 16:06:42 -0700

    Person 2 updated 2

--- Person 2/src/person/pkg2/Person2.java
+++ Person 2/src/person/pkg2/Person2.java
@@ -18,12 +18,69 @@ public class Person2 {
         boolean isAStudent ;
         String phoneNumber ;
         String email ;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isIsAStudent() {
+        return isAStudent;
+    }
+
+    public void setIsAStudent(boolean isAStudent) {
+        this.isAStudent = isAStudent;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
        
         
     
     public Person2(String fn, String ln, String pn) {
-          String firstName ;
-        String lastName  ;
-        String phoneNumber ;
+          String firstName=fn;
+        String lastName=ln  ;
+        String phoneNumber=pn ;
     }
+    
 }
