dd7b0453e7e664d64713d2f2d224b044921d9320
BobTomBob <josephbsilvi@gmail.com>
2017-07-27 19:48:15 -0700

    calc2

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -22,28 +22,68 @@ public class Calculator {
         System.out.println("Type an equation to proceed. You can type quit now if you wish to quit ");
         while (true) {
             String input = keyboard.nextLine();
-            if (input.equals("quit") || (input.equals("Quit")) || (input.equals("qUiz")) || (input.equals("QUit")) || (input.equals("QUIt")) || (input.equals("QUIT")) || (input.equals("qUIt")) || (input.equals("qUIT")) || (input.equals("QuiT")) || (input.equals("QuIT")) || (input.equals("QuIt"))) {
+            if (input.equals("quit") || (input.equals("Quit")) || (input.equals("qUiz")) || (input.equals("QUit")) || (input.equals("QUIt")) || (input.equals("QUIT")) || (input.equals("qUIt")) || (input.equals("qUIT")) || (input.equals("QuiT")) || (input.equals("QuIT")) || (input.equals("QuIt")) || input.equals("q")) {
                 break;
             }
 
             //logic: calculate input
-            if (input.contains("+") || input.contains("t")) {
+            if (input.contains("+") || input.contains("T")) {
                 int index = input.indexOf("+");
-                if (index==-1) {
-                     index = input.indexOf("t");
+                if (index == -1) {
+                    index = input.indexOf("T");
                 }
-                
+
                 String num1 = input.substring(0, index);
-                String num2 = input.substring(index+1, input.length());
-                double addition = Double.parseDouble(num1 )+ Double.parseDouble( num2);
+                String num2 = input.substring(index + 1, input.length());
+                double addition = Double.parseDouble(num1) + Double.parseDouble(num2);
                 System.out.println(addition);
-                
-                
+
             } else if (input.contains("-") || input.contains("_")) {
+                int index = input.indexOf("-");
+                if (index == -1) {
+                    index = input.indexOf("_");
+                }
+
+                String num1 = input.substring(0, index);
+                String num2 = input.substring(index + 1, input.length());
+                double subtraction = Double.parseDouble(num1) - Double.parseDouble(num2);
+                System.out.println(subtraction);
 
             } else if (input.contains("*") || input.contains("x") || input.contains("X")) {
+                int index = input.indexOf("*");
+                if (index == -1) {
+                    index = input.indexOf("x");
+                }
+                if (index == -1) {
+                    index = input.indexOf("X");
+                }
+                String num1 = input.substring(0, index);
+                String num2 = input.substring(index + 1, input.length());
+                double multiplication = Double.parseDouble(num1) * Double.parseDouble(num2);
+                System.out.println(multiplication);
 
-            } else if (input.contains("/") || input.contains("÷") ) {
+            } else if (input.contains("/") || input.contains("÷")) {
+                int index = input.indexOf("/");
+                if (index == -1) {
+                    index = input.indexOf("÷");
+                }
+
+                String num1 = input.substring(0, index);
+                String num2 = input.substring(index + 1, input.length());
+                double division = Double.parseDouble(num1) / Double.parseDouble(num2);
+                System.out.println(division);
+
+            } else if (input.contains("z") || input.contains("sqrt")) {
+                int index = input.indexOf("z");
+                if (index == -1) {
+                    index = input.indexOf("t");
+                }
+
+                String num2 = input.substring(index + 1, input.length());
+                double squareroot = Double.parseDouble(num2);
+                double sqrt = Math.sqrt(squareroot);
+                System.out.println(sqrt);
+
             }
         }
     }
